419d6c5ffb13611942c6120d484978093bb6c431
Rahul Ramjeawon <96823012+CodeWithTejpratab@users.noreply.github.com>
2024-09-27 19:18:30 -0400

    Update .gitignore
    Project files
    
    Update file structure
    
    update files
    
    added project

---
+++ Orchen/Orchen/View/OrchenApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrchenApp.swift
+//  Orchen
+//
+//  Created by Rahul Ramjeawon on 9/27/24.
+//
+
+import SwiftUI
+
+@main
+struct OrchenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
